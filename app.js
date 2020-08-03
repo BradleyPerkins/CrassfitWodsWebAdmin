1b006e825e88d8c0f2e6660d2bf43df75aad8e45
Bradley Perkins <bperkins99@gmail.com>
2020-08-03 07:16:38 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyArTEY0yV026edAMskmeiqYVvVwiLDWakA",
+    authDomain: "crossfitwods-9ca4e.firebaseapp.com",
+    databaseURL: "https://crossfitwods-9ca4e.firebaseio.com",
+    projectId: "crossfitwods-9ca4e",
+    storageBucket: "crossfitwods-9ca4e.appspot.com",
+    messagingSenderId: "375987226984",
+    appId: "1:375987226984:web:642c833ae90fbb6bb26edd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+//   var email = "bperkins99@gmail.com";
+//   var password = "1111111";
+
+//           // [START authwithemail]
+//           firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//             // Handle Errors here.
+//             var errorCode = error.code;
+//             var errorMessage = error.message;
+//             // [START_EXCLUDE]
+//             if (errorCode === 'auth/wrong-password') {
+//               alert('Wrong password.');
+//             } else {
+//               alert(errorMessage);
+//             }
+//             console.log(error);
+//             document.getElementById('quickstart-sign-in').disabled = false;
+//             // [END_EXCLUDE]
+//           });
+//           // [END authwithemail]
+  
+
+    // auth.signInWithEmailAndPassword(email, pw)
+    //     .then(function(firebaseUser) {
+    //         // Success 
+    //     })
+    //     .catch(function(error) {
+    //         // Error Handling
+    // });
+
+
+
+
+
+//var storage = firebase.storage();
+
+
